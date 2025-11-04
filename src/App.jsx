82bd0fc3c6632ee5305f76a08e2312e9b6a4a0e7
stefanobellomo <stefanobellomo1998@gmail.com>
2025-11-04 14:53:26 +0100

    create a header component and import it in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import dc_logo from './assets/img/dc-logo.png'
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
-      <header>
-        <div><img src={dc_logo} alt="logo" /></div>
-        <nav className="nav justify-content-center">
-          <a className="nav-link" href="#">CHARACTERS</a>
-          <a className="nav-link" href="#">COMICS</a>
-          <a className="nav-link" href="#">MOVIES</a>
-          <a className="nav-link" href="#">TV</a>
-          <a className="nav-link" href="#">GAMES</a>
-          <a className="nav-link" href="#">COLLECTIBLES</a>
-          <a className="nav-link" href="#">VIDEOS</a>
-          <a className="nav-link" href="#">NEWS</a>
-          <a className="nav-link" href="#">FANS</a>
-          <a className="nav-link" href="#">SHOP</a>
-        </nav>
+      <Header />
 
-      </header>
     </>
   )
 }

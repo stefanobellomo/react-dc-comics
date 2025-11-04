c86b6426280da4d570694722aa403ec560145f39
stefanobellomo <stefanobellomo1998@gmail.com>
2025-11-04 14:35:14 +0100

    create header with img and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-
+import dc_logo from './assets/img/dc-logo.png'
 
 function App() {
 
   return (
     <>
-      <header>ciao</header>
+      <header>
+        <div><img src={dc_logo} alt="logo" /></div>
+        <nav className="nav justify-content-center">
+          <a className="nav-link" href="#">CHARACTERS</a>
+          <a className="nav-link" href="#">COMICS</a>
+          <a className="nav-link" href="#">MOVIES</a>
+          <a className="nav-link" href="#">TV</a>
+          <a className="nav-link" href="#">GAMES</a>
+          <a className="nav-link" href="#">COLLECTIBLES</a>
+          <a className="nav-link" href="#">VIDEOS</a>
+          <a className="nav-link" href="#">NEWS</a>
+          <a className="nav-link" href="#">FANS</a>
+          <a className="nav-link" href="#">SHOP</a>
+        </nav>
+
+      </header>
     </>
   )
 }
